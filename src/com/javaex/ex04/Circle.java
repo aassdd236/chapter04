3959682b5a8d1ea7759ddee1c025ca726b1d99d7
aassdd236 <aassdd236@naver.com>
2024-01-12 16:25:59 +0900

    01/12

---
+++ src/com/javaex/ex04/Circle.java
@@ -0,0 +1,31 @@
+package com.javaex.ex04;
+
+public class Circle {
+	
+	private int radius;
+
+	public Circle(int radius) {
+		super();
+		this.radius = radius;
+	}
+	public Circle() {
+	}
+	
+	public int getRadius() {
+		return radius;
+	}
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	
+	@Override
+	public String toString() {
+		return "Circle [radius=" + radius + "]";
+	}
+	
+	
+	
+	
+	
+
+}
